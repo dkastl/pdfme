4b406a653f9e3d5567d80c0a62ddd5f7374e4c55
Daniel Kastl <daniel@georepublic.de>
2023-12-06 16:57:47 +0900

    Tweaking the delay timout
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- playground/src/plugins/map/helper.ts
+++ playground/src/plugins/map/helper.ts
@@ -91,67 +91,81 @@ export const setupMap = (mapDiv: HTMLElement, mode: string, key: string) => {
   return map;
 };
 
-export const updateMap = (map: any, key: string, onChange: Function) => {
-  setTimeout(() => {
-    generateDataUrl(map, key, onChange);
-  }, 500);
+let debounceTimer: any;
+
+export const updateMap = (map: any, key: string, onChange: Function, debounceTime = 1000) => {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(() => {
+    generateDataUrl(map, key)
+      .then(dataUrl => {
+        if (onChange) {
+          onChange(dataUrl);
+        }
+      })
+      .catch(error => console.error(error));
+  }, debounceTime);
 };
 
-const generateDataUrl = (map: any, key: string, onChange: Function) => {
-  const size = map.getSize();
-  if (!size) {
-    return;
-  }
+const generateDataUrl = (map: any, key: string) => {
+  return new Promise((resolve, reject) => {
+    // Delay to ensure map tiles are loaded
+    setTimeout(() => {
+      const size = map.getSize();
+      if (!size) {
+        reject('Map size not available');
+        return;
+      }
 
-  const exportCanvas = document.createElement('canvas');
-  exportCanvas.width = size[0];
-  exportCanvas.height = size[1];
-  const exportContext = exportCanvas.getContext('2d');
+      const exportCanvas = document.createElement('canvas');
+      exportCanvas.width = size[0];
+      exportCanvas.height = size[1];
+      const exportContext = exportCanvas.getContext('2d');
 
-  if (exportContext) {
-    // Draw each canvas from the map onto the export canvas
-    document.querySelectorAll('.ol-layer canvas').forEach(function (canvasElement) {
-      const canvas = canvasElement as HTMLCanvasElement;
-      if (canvas.width > 0) {
-        const parentNode = canvas.parentNode as HTMLElement;
-        if (parentNode) {
-          exportContext.globalAlpha = parentNode.style.opacity === '' ? 1 : Number(parentNode.style.opacity);
-          const transform = canvas.style.transform;
-          const match = transform.match(/^matrix\(([^\(]*)\)$/);
-          if (match) {
-            const matrix = match[1].split(',').map(Number);
-            // Apply matrix values to setTransform method
-            exportContext.setTransform(matrix[0], matrix[1], matrix[2], matrix[3], matrix[4], matrix[5]);
-            exportContext.drawImage(canvas, 0, 0);
+      if (!exportContext) {
+        reject('Failed to get canvas context');
+        return;
+      }
+
+      // Draw each canvas from the map onto the export canvas
+      document.querySelectorAll('.ol-layer canvas').forEach(function (canvasElement) {
+        const canvas = canvasElement as HTMLCanvasElement;
+        if (canvas.width > 0) {
+          const parentNode = canvas.parentNode as HTMLElement;
+          if (parentNode) {
+            exportContext.globalAlpha = parentNode.style.opacity === '' ? 1 : Number(parentNode.style.opacity);
+            const transform = canvas.style.transform;
+            const match = transform.match(/^matrix\(([^\(]*)\)$/);
+            if (match) {
+              const matrix = match[1].split(',').map(Number);
+              // Apply matrix values to setTransform method
+              exportContext.setTransform(matrix[0], matrix[1], matrix[2], matrix[3], matrix[4], matrix[5]);
+              exportContext.drawImage(canvas, 0, 0);
+            }
           }
         }
-      }
-    });
+      });
 
-    // Reset transformations and alpha for the export canvas
-    exportContext.globalAlpha = 1;
-    exportContext.setTransform(1, 0, 0, 1, 0, 0);
-  
-    // Get the dataUrl from the export canvas
-    const data = exportCanvas.toDataURL('image/png');
+      // Reset transformations and alpha for the export canvas
+      exportContext.globalAlpha = 1;
+      exportContext.setTransform(1, 0, 0, 1, 0, 0);
+
+      // Get the dataUrl from the export canvas
+      const data = exportCanvas.toDataURL('image/png');
+      const currentState = mapStates[key] || { center: [0, 0], zoom: 2, dataUrl: '' };
 
     // Only update and trigger onChange if the dataUrl has changed
-    const currentState = mapStates[key] || { center: [0, 0], zoom: 2, dataUrl: '' };
     if (currentState.dataUrl !== data) {
-      const view = map.getView();
-      const newState: MapState = {
-        center: view.getCenter() as [number, number],
-        zoom: view.getZoom() as number,
-        dataUrl: data
-      };
-      saveMapState(key, newState);
-      if (onChange) {
-        onChange(newState.dataUrl);
+        const view = map.getView();
+        const newState: MapState = {
+          center: view.getCenter() as [number, number],
+          zoom: view.getZoom() as number,
+          dataUrl: data
+        };
+        saveMapState(key, newState);
+        resolve(newState.dataUrl);
+      } else {
+        resolve(currentState.dataUrl);
       }
-      else {
-        // propPanel.defaultValue = newState.dataUrl;
-        console.log('onChange is null');
-      }
-    }
-  };
-}
+    }, 500);
+  });
+};
